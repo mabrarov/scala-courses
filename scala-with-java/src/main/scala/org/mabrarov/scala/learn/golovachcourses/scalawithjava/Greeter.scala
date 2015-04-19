94c4bb3dc8e45cb6d4b58522ac5e96f5646006fa
Marat Abrarov <abrarov@gmail.com>
2015-04-19 13:50:43 +0300

    Minor renaming.

--- scala-with-java/src/main/scala/org/mabrarov/scala/learn/golovachcourses/scalawithjava/Greeter.scala
+++ scala-with-java/src/main/scala/org/mabrarov/scala/learn/golovachcourses/scalawithjava/Greeter.scala
@@ -3,7 +3,7 @@ package org.mabrarov.scala.learn.golovachcourses.scalawithjava
 class Greeter {
 
   def greet(name: String) = {
-    val myJavaClass = new GreetingBuilder()
-    println(myJavaClass.buildGreeting(name))
+    val greetingBuilder = new GreetingBuilder()
+    println(greetingBuilder.buildGreeting(name))
   }
 }
